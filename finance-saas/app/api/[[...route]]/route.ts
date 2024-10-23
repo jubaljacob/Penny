49633098390dceb796378e30eb765f54e02170ed
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2024-10-23 12:04:19 +0530

    hono rpc

--- finance-saas/app/api/[[...route]]/route.ts
+++ finance-saas/app/api/[[...route]]/route.ts
@@ -1,11 +1,19 @@
 import { Hono } from 'hono';
-
 import { handle } from 'hono/vercel';
 import accounts from './account';
+import { HTTPException } from 'hono/http-exception';
+
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
+
+app.onError((err,c) => {
+    if(err instanceof HTTPException){
+        return err.getResponse();
+    }
+    return c.json({ error: "internal error"},500)
+})
 
 const routes = app.route("/accounts",accounts)
 
--- finance-saas/app/layout.tsx
+++ finance-saas/app/layout.tsx
@@ -39,9 +39,7 @@ export default function RootLayout({
       <html lang="en">
         <body className="{inter.className">
           <QueryProvider>
-
           {children}
-
           </QueryProvider>
         </body>
       </html>
